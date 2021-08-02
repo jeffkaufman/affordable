6b20cc594608b6f28ef0b3539de539dd2f5ea7d1
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2021-08-02 16:01:52 +0000

    track occupancy (units)

--- process-parcels.py
+++ process-parcels.py
@@ -12,6 +12,7 @@ class ParcelParser(HTMLParser):
       "MainContent_lblZone": None,
       "MainContent_lblLocation": None,
     }
+    self.occupancy = 0
 
   def record(self):
     for k, v in self.values.items():
@@ -27,20 +28,32 @@ class ParcelParser(HTMLParser):
     score = 0
     if sqft > 0 and zone in ['MR4', 'MR5', 'MR6']:
       score = assessment / sqft
-    return [score, self.parcel_id, pretty_assessment, sqft, zone, address]
+    return [score, self.parcel_id, pretty_assessment, sqft, zone, address, self.occupancy]
 
   def handle_starttag(self, tag, attrs):
     if tag == "span":
       for k, v in attrs:
         if k == "id" and v in self.values:
           self.state = v
+    elif tag == "td":
+      if self.state == "occupancy":
+        self.state = "read-occupancy"
+      else:
+        self.state = "td"
 
   def handle_endtag(self, tag):
-    self.state = "initial"
+    if self.state not in ["occupancy"]:
+      self.state = "initial"
 
   def handle_data(self, data):
     if self.state in self.values:
       self.values[self.state] = data
+    elif self.state == "td" and data == "Occupancy":
+      self.state = "occupancy"
+    elif self.state == "read-occupancy":
+      data = data.strip()
+      if data:
+        self.occupancy = int(data)
 
 def get_records():
   records = []
@@ -52,16 +65,17 @@ def get_records():
         continue
       parser.feed(html)
       record = parser.record()
-      records.append(parser.record())
+      records.append(record)
+
   return records
 
 def print_records(records):
-  print("valuation per sqft\taddress\tzone\tsqft\tvaluation\tlink")
-  for score, parcel_id, pretty_assessment, sqft, zone, address in records:
+  print("valuation per sqft\taddress\tzone\tsqft\tvaluation\tlink\toccupancy")
+  for score, parcel_id, pretty_assessment, sqft, zone, address, occupancy in records:
     if score < 1:
       continue
-    print("%.0f\t%s\t%s\t%s\t%s\thttps://gis.vgsi.com/somervillema/Parcel.aspx?Pid=%s" % (
-      score, address, zone, sqft, pretty_assessment, parcel_id
+    print("%.0f\t%s\t%s\t%s\t%s\thttps://gis.vgsi.com/somervillema/Parcel.aspx?Pid=%s\t%s" % (
+      score, address, zone, sqft, pretty_assessment, parcel_id, occupancy
     ))
     #print("%.2f: %s (#%s) -- %s @ %s in %s" % (
     #  score, address, parcel_id, sqft, pretty_assessment, zone))
